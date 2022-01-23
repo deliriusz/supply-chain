b3982f4b45630cdf4b5eadfef3a91669e3e2671e
Rafal Kalinowski <kalinowskirafal.fm@gmail.com>
2022-01-23 21:57:36 +0100

    working on admin pacges - statistics

--- frontend/src/components/admin/Dashboard.tsx
+++ frontend/src/components/admin/Dashboard.tsx
@@ -1,14 +1,169 @@
-import React from "react";
+import React, { useState } from "react";
+import { Icon, Menu, MenuItemProps, Segment, Statistic } from "semantic-ui-react";
+import SalesChart from "./SalesChart";
 
-class Dashboard extends React.Component {
-   render() {
-      return (
-         <>
-            <h3>contract a product button</h3>
-            <p>list of all products with filtering</p>
-         </>
-      )
-   }
+//TODO: think about making menu sticky
+const Dashboard = () => {
+
+   const [activeItem, setActiveItem] = useState<string>('statistics');
+   const handleItemClick = (event: React.MouseEvent<HTMLAnchorElement>, data: MenuItemProps) => { setActiveItem(data.name ? data.name : 'statistics') };
+
+   return (
+      <>
+         <Menu pointing secondary>
+            <Menu.Item
+               name='statistics'
+               active={activeItem === 'statistics'}
+               onClick={handleItemClick}
+            />
+            <Menu.Item
+               name='sales'
+               active={activeItem === 'sales'}
+               onClick={handleItemClick}
+            />
+            <Menu.Item
+               name='product search'
+               active={activeItem === 'product search'}
+               onClick={handleItemClick}
+            />
+         </Menu>
+         {activeItem}
+         <Segment compact>
+            <Statistic.Group>
+               <Statistic>
+                  <Statistic.Value>22</Statistic.Value>
+                  <Statistic.Label>Saves</Statistic.Label>
+               </Statistic>
+
+               <Statistic>
+                  <Statistic.Value text>
+                     Three
+                     <br />
+                     Thousand
+                  </Statistic.Value>
+                  <Statistic.Label>Signups</Statistic.Label>
+               </Statistic>
+
+               <Statistic>
+                  <Statistic.Value>
+                     <Icon name='plane' />5
+                  </Statistic.Value>
+                  <Statistic.Label>Flights</Statistic.Label>
+               </Statistic>
+
+               <Statistic>
+                  <Statistic.Value>
+                     42
+                  </Statistic.Value>
+                  <Statistic.Label>Team Members</Statistic.Label>
+               </Statistic>
+            </Statistic.Group>
+         </Segment>
+         <SalesChart />
+         <h3>contract a product button</h3>
+         <p>list of all products with filtering</p>
+         <p>list of all products with filtering</p>
+         <p>list of all products with filtering</p>
+         <p>list of all products with filtering</p>
+         <p>list of all products with filtering</p>
+         <p>list of all products with filtering</p>
+         <p>list of all products with filtering</p>
+         <p>list of all products with filtering</p>
+         <p>list of all products with filtering</p>
+         <p>list of all products with filtering</p>
+         <p>list of all products with filtering</p>
+         <p>list of all products with filtering</p>
+         <p>list of all products with filtering</p>
+         <p>list of all products with filtering</p>
+         <p>list of all products with filtering</p>
+         <p>list of all products with filtering</p>
+         <p>list of all products with filtering</p>
+         <p>list of all products with filtering</p>
+         <p>list of all products with filtering</p>
+         <p>list of all products with filtering</p>
+         <p>list of all products with filtering</p>
+         <p>list of all products with filtering</p>
+         <p>list of all products with filtering</p>
+         <p>list of all products with filtering</p>
+         <p>list of all products with filtering</p>
+         <p>list of all products with filtering</p>
+         <p>list of all products with filtering</p>
+         <p>list of all products with filtering</p>
+         <p>list of all products with filtering</p>
+         <p>list of all products with filtering</p>
+         <p>list of all products with filtering</p>
+         <p>list of all products with filtering</p>
+         <p>list of all products with filtering</p>
+         <p>list of all products with filtering</p>
+         <p>list of all products with filtering</p>
+         <p>list of all products with filtering</p>
+         <p>list of all products with filtering</p>
+         <p>list of all products with filtering</p>
+         <p>list of all products with filtering</p>
+         <p>list of all products with filtering</p>
+         <p>list of all products with filtering</p>
+         <p>list of all products with filtering</p>
+         <p>list of all products with filtering</p>
+         <p>list of all products with filtering</p>
+         <p>list of all products with filtering</p>
+         <p>list of all products with filtering</p>
+         <p>list of all products with filtering</p>
+         <p>list of all products with filtering</p>
+         <p>list of all products with filtering</p>
+         <p>list of all products with filtering</p>
+         <p>list of all products with filtering</p>
+         <p>list of all products with filtering</p>
+         <p>list of all products with filtering</p>
+         <p>list of all products with filtering</p>
+         <p>list of all products with filtering</p>
+         <p>list of all products with filtering</p>
+         <p>list of all products with filtering</p>
+         <p>list of all products with filtering</p>
+         <p>list of all products with filtering</p>
+         <p>list of all products with filtering</p>
+         <p>list of all products with filtering</p>
+         <p>list of all products with filtering</p>
+         <p>list of all products with filtering</p>
+         <p>list of all products with filtering</p>
+         <p>list of all products with filtering</p>
+         <p>list of all products with filtering</p>
+         <p>list of all products with filtering</p>
+         <p>list of all products with filtering</p>
+         <p>list of all products with filtering</p>
+         <p>list of all products with filtering</p>
+         <p>list of all products with filtering</p>
+         <p>list of all products with filtering</p>
+         <p>list of all products with filtering</p>
+         <p>list of all products with filtering</p>
+         <p>list of all products with filtering</p>
+         <p>list of all products with filtering</p>
+         <p>list of all products with filtering</p>
+         <p>list of all products with filtering</p>
+         <p>list of all products with filtering</p>
+         <p>list of all products with filtering</p>
+         <p>list of all products with filtering</p>
+         <p>list of all products with filtering</p>
+         <p>list of all products with filtering</p>
+         <p>list of all products with filtering</p>
+         <p>list of all products with filtering</p>
+         <p>list of all products with filtering</p>
+         <p>list of all products with filtering</p>
+         <p>list of all products with filtering</p>
+         <p>list of all products with filtering</p>
+         <p>list of all products with filtering</p>
+         <p>list of all products with filtering</p>
+         <p>list of all products with filtering</p>
+         <p>list of all products with filtering</p>
+         <p>list of all products with filtering</p>
+         <p>list of all products with filtering</p>
+         <p>list of all products with filtering</p>
+         <p>list of all products with filtering</p>
+         <p>list of all products with filtering</p>
+         <p>list of all products with filtering</p>
+         <p>list of all products with filtering</p>
+         <p>list of all products with filtering</p>
+      </>
+   )
 }
 
 export default Dashboard;
---
+++ frontend/src/components/admin/SalesChart.tsx
@@ -0,0 +1,58 @@
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js'
+import { Chart } from 'react-chartjs-2'
+
+import React, { useState } from "react"
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+)
+
+const labels = [
+   'January',
+   'February',
+   'March',
+   'April',
+   'May',
+   'June',
+];
+const data = {
+   labels: labels,
+   datasets: [{
+      label: 'My First dataset',
+      backgroundColor: 'rgb(255, 99, 132)',
+      borderColor: 'rgb(255, 99, 132)',
+      data: [0, 10, 5, 2, 20, 30, 45],
+   }]
+};
+
+const config = {
+   type: 'line',
+   data: data,
+   options: {}
+};
+
+const SalesChart = () => {
+
+   return (
+      <div>
+      <Chart type='line' data={data} />
+      </div>
+   )
+}
+
+export default SalesChart
