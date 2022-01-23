5180ca3f516ce62242d7a1084f72a0fde14f1da2
Rafal Kalinowski <kalinowskirafal.fm@gmail.com>
2022-01-23 22:03:18 +0100

    cleanup + setting lineTension

--- frontend/src/components/admin/SalesChart.tsx
+++ frontend/src/components/admin/SalesChart.tsx
@@ -30,20 +30,16 @@ const labels = [
    'May',
    'June',
 ];
+
 const data = {
    labels: labels,
    datasets: [{
       label: 'My First dataset',
       backgroundColor: 'rgb(255, 99, 132)',
       borderColor: 'rgb(255, 99, 132)',
+      lineTension: 0.3,
       data: [0, 10, 5, 2, 20, 30, 45],
    }]
-};
-
-const config = {
-   type: 'line',
-   data: data,
-   options: {}
 };
 
 const SalesChart = () => {
