783ca5eae4b535f407009d647cd621ffc34dd8a7
Rafal Kalinowski <kalinowskirafal.fm@gmail.com>
2022-03-30 21:01:37 +0200

    refactoring DB code for product and purchase

--- backend/repository/product.go
+++ backend/repository/product.go
@@ -1,9 +1,15 @@
 package repository
 
 import (
+	"bytes"
+	"fmt"
+	"math/rand"
 	"mime/multipart"
 	"os"
+	"time"
 
+	"github.com/ethereum/go-ethereum/crypto"
+	"rafal-kalinowski.pl/config"
 	"rafal-kalinowski.pl/domain/model"
 	domain "rafal-kalinowski.pl/domain/service"
 )
@@ -21,17 +27,78 @@ func NewProductRepository(c RepoConnector) domain.ProductRepository {
 }
 
 func (repo *productRepository) GetProducts(limit, offset uint) []model.Product {
+	var products []model.Product
 
+	count := 0
+	model.DB.Find(&[]model.Product{}).Count(&count)
+
+	model.DB.Preload("Img").Preload("Specification").
+		Scopes(Paginate(int(limit), int(offset))).
+		Find(&products)
+
+	return products
 }
+
 func (repo *productRepository) GetProduct(id uint) (model.Product, error) {
+	product, err := getProduct(id)
 
+	return product, err
 }
-func (repo *productRepository) CreateProduct(*model.Product) error {
 
+func (repo *productRepository) CreateProduct(product *model.Product) error {
+	if err := model.DB.Create(&product).Error; err != nil {
+		return err
+	}
+
+	for _, sd := range product.Specification {
+		model.DB.Create(&sd)
+	}
+
+	return nil
 }
+
 func (repo *productRepository) CreateImage(productId uint, file *multipart.File) error {
+	if _, err := getProduct(productId); err != nil {
+		return err
+	}
 
+	imageNameBase := fmt.Sprintf("%d-%d-%d", productId, time.Now().UnixNano(), rand.Int63())
+	imageNameKeccak := crypto.Keccak256([]byte(imageNameBase))
+
+	image := model.Image{
+		ProductId: uint(productId),
+		ImageName: string(imageNameKeccak),
+	}
+
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(*file)
+
+	// err = nil
+	if err := os.WriteFile(config.IMAGE_LOCAL_STORAGE+image.ImageName, buf.Bytes(), 0644); err != nil {
+		return err
+	}
+
+	if err := model.DB.Create(&image).Error; err != nil {
+		return err
+	}
+
+	return nil
 }
-func (repo *productRepository) GetImage(fileName string) (name, dir string, file *os.File) {
 
+func (repo *productRepository) GetImage(fileName string) (string, string, *os.File, error) {
+	directory := config.IMAGE_LOCAL_STORAGE
+	file, err := os.Open(directory + "/" + fileName)
+
+	return fileName, directory, file, err
+}
+
+func getProduct(productId uint) (model.Product, error) {
+	var product model.Product
+
+	if err := model.DB.Where("id = ?",
+		productId).Preload("Img").Preload("Specification").First(&product, productId).Error; err != nil {
+		return model.Product{}, err
+	}
+
+	return product, nil
 }
--- backend/repository/purchase.go
+++ backend/repository/purchase.go
@@ -17,10 +17,48 @@ func NewPurchaseRepository(c RepoConnector) domain.PurchaseRepository {
 	return repo
 }
 
-func (repo *purchaseRepository) GetPurchases(limit, offset uint) []model.PurchaseOrder {}
-func (repo *purchaseRepository) GetPurchase(id uint) (model.PurchaseOrder, error)      {}
-func (repo *purchaseRepository) CreatePurchase(*model.PurchaseOrder) error {
+func (repo *purchaseRepository) GetPurchases(limit, offset uint) []model.PurchaseOrder {
+	var purchase []model.PurchaseOrder
 
+	count := 0
+	model.DB.Find(&[]model.PurchaseOrder{}).Count(&count)
+
+	model.DB.Preload("Product").
+		Scopes(Paginate(int(limit), int(offset))).
+		Find(&purchase)
+
+	return purchase
 }
+
+func (repo *purchaseRepository) GetPurchase(id uint) (model.PurchaseOrder, error) {
+	var purchase model.PurchaseOrder
+
+	if err := model.DB.Where("id = ?", id).Preload("Product").
+		First(&purchase, id).Error; err != nil {
+		return purchase, err
+	}
+
+	return purchase, nil
+}
+
+func (repo *purchaseRepository) CreatePurchase(purchase *model.PurchaseOrder) error {
+	if err := model.DB.Create(&purchase).Error; err != nil {
+		return err
+	}
+
+	return nil
+}
+
 func (repo *purchaseRepository) GetPurchasesForUser(limit, offset uint, user string) []model.PurchaseOrder {
+	var purchase []model.PurchaseOrder
+	count := 0
+
+	model.DB.Where("user_id = ?", user).Find(&[]model.PurchaseOrder{}).Count(&count)
+
+	model.DB.Preload("Product").
+		Scopes(Paginate(int(limit), int(offset))).
+		Where("id = ?", user).Find(&[]model.PurchaseOrder{})
+
+	model.DB.Where("user_id = ?", user).Preload("Product").Find(&purchase)
+	return purchase
 }
