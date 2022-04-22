466a93689f14489da06a1d3b04c2f05cac63f435
Rafal Kalinowski <kalinowskirafal.fm@gmail.com>
2022-04-22 12:17:08 +0200

    moving db repo connector to separate file

---
+++ backend/repository/db-repo-connector.go
@@ -0,0 +1,55 @@
+package repository
+
+import (
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
+
+	"rafal-kalinowski.pl/domain/model"
+)
+
+type DBRepoConnector struct {
+	DB *gorm.DB
+}
+
+// GetConnector implements RepoConnector
+func (c *DBRepoConnector) GetConnector() *DBRepoConnector {
+	return c
+}
+
+// InitConnection implements RepoConnector
+func (c *DBRepoConnector) InitConnection(name, connectionString string) error {
+	database, err := gorm.Open("sqlite3", name)
+
+	if err != nil {
+		panic(err)
+	}
+
+	x := EthereumRepoConnector{}
+	x.GetConnector()
+
+	database.AutoMigrate(&model.Image{})
+	database.AutoMigrate(&model.Specification{})
+	database.AutoMigrate(&model.Product{})
+	database.AutoMigrate(&model.PurchaseOrder{})
+	database.AutoMigrate(&model.Login{})
+
+	c.DB = database
+
+	return err
+}
+
+func Paginate(limit, offset int) func(db *gorm.DB) *gorm.DB {
+	return func(db *gorm.DB) *gorm.DB {
+		if limit <= 0 {
+			limit = 10
+		} else if limit > 100 {
+			limit = 100
+		}
+
+		if offset < 0 {
+			offset = 0
+		}
+
+		return db.Offset(offset).Limit(limit)
+	}
+}
--- backend/repository/repo-connector.go
+++ backend/repository/repo-connector.go
@@ -3,10 +3,7 @@ package repository
 import (
 	"reflect"
 
-	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
-
-	"rafal-kalinowski.pl/domain/model"
 )
 
 var ProviderFactory *DataProviderFactory
@@ -39,62 +36,12 @@ type RepoConnector[T RepoType] interface {
 	GetConnector() T
 }
 
-type DBRepoConnector struct {
-	DB *gorm.DB
-}
-
 // GetConnector implements RepoConnector
-func (c *DBRepoConnector) GetConnector() *DBRepoConnector {
-	return c
-}
-
-// InitConnection implements RepoConnector
-func (c *DBRepoConnector) InitConnection(name, connectionString string) error {
-	database, err := gorm.Open("sqlite3", name)
-
-	if err != nil {
-		panic(err)
-	}
-
-	x := EthereumRepoConnector{}
-	x.GetConnector()
-
-	database.AutoMigrate(&model.Image{})
-	database.AutoMigrate(&model.Specification{})
-	database.AutoMigrate(&model.Product{})
-	database.AutoMigrate(&model.PurchaseOrder{})
-	database.AutoMigrate(&model.Login{})
-
-	c.DB = database
-
-	return err
-}
 
 // func NewRepoConnector[T RepoType](v T) RepoConnector[T] {
 func NewRepoConnector[T RepoType](v T) RepoConnector[*EthereumRepoConnector] {
 	x := EthereumRepoConnector{}
 	return &x
-	// return &v
-}
-
-// func NewRepoConnector[T RepoType](v T) RepoConnector[T] {
-// 	return v
-// }
-
-func Paginate(limit, offset int) func(db *gorm.DB) *gorm.DB {
-	return func(db *gorm.DB) *gorm.DB {
-		if limit <= 0 {
-			limit = 10
-		} else if limit > 100 {
-			limit = 100
-		}
-
-		if offset < 0 {
-			offset = 0
-		}
-
-		return db.Offset(offset).Limit(limit)
-	}
 }
 
 func init() {
