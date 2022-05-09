6832d7f78c6dd6834082fe4d6d7b75c6c1e6886d
Rafal Kalinowski <kalinowskirafal.fm@gmail.com>
2022-05-09 21:33:53 +0200

    renaming getproductmodels local interface

--- frontend/src/services/ProductService.ts
+++ frontend/src/services/ProductService.ts
@@ -2,13 +2,13 @@ import ProductModel from "../interfaces/ProductModel";
 import ResponseContent from "../interfaces/ResponseContent";
 import { callService } from "./ServiceBase";
 
-interface GetProductsResponse {
+interface GetProductModelsResponse {
    total: number,
    products: ProductModel[]
 }
 
-const getProductModels = async (offset: number = 0, limit: number = 10): Promise<GetProductsResponse> => {
-   return callService<GetProductsResponse>(`product-model?offset=${offset}&limit=${limit}`)
+const getProductModels = async (offset: number = 0, limit: number = 10): Promise<GetProductModelsResponse> => {
+   return callService<GetProductModelsResponse>(`product-model?offset=${offset}&limit=${limit}`)
       .then(response => {
          return {
             total: response.data?.total || 0,
