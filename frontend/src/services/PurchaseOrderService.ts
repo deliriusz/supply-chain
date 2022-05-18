2a3664dec9f133ef878e0fcfae2d6f50d6a32e83
Rafal Kalinowski <kalinowskirafal.fm@gmail.com>
2022-05-18 13:18:40 +0200

    mocking purchase order service call

--- frontend/src/services/PurchaseOrderService.ts
+++ frontend/src/services/PurchaseOrderService.ts
@@ -8,7 +8,71 @@ interface GetPurchaseOrdersResponse {
 }
 
 //TODO: map userId
-const getPurchaseOrdersForUser = async (userId: number, offset: number = 0, limit: number = 10): Promise<GetPurchaseOrdersResponse> => {
+const getPurchaseOrdersForUser = async (userId: string, offset: number = 0, limit: number = 10): Promise<GetPurchaseOrdersResponse> => {
+   return {
+      total: 12,
+      products: [
+         {
+            id: "1",
+            userId: "",
+            date: "2020-05-13 15:23:58",
+            price: 100,
+            product: [
+               {
+                  price: 90,
+                  state: "IN_PROGRESS",
+                  productModel: {
+                     description: "",
+                     title: "product title",
+                     id: 1,
+                     images: [],
+                     specification: [],
+                     quantity: 1,
+                     price: 100
+                  }
+               },
+               {
+                  price: 10,
+                  state: "PAYED",
+                  productModel: {
+                     description: "",
+                     title: "product title 2",
+                     id: 1,
+                     images: [],
+                     specification: [],
+                     quantity: 1,
+                     price: 100
+                  }
+               }
+            ],
+            status: "IN_PROGRESS"
+
+         },
+         {
+            id: "2",
+            userId: "",
+            date: "2021-12-24 09:30:00",
+            price: 250,
+            product: [
+               {
+                  price: 250,
+                  state: "DELIVERED",
+                  productModel: {
+                     description: "",
+                     title: "product title 3",
+                     id: 1,
+                     images: [],
+                     specification: [],
+                     quantity: 1,
+                     price: 100
+                  }
+               }
+            ],
+            status: "DELIVERED"
+
+         }
+      ]
+   }
 
    return callService<GetPurchaseOrdersResponse>(`purchase?offset=${offset}&limit=${limit}`)
       .then(response => {
