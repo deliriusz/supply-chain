e012263b6c3d2479afa32112097a47205dfa6db8
Rafal Kalinowski <kalinowskirafal.fm@gmail.com>
2022-05-25 13:49:15 +0200

    prototyping main page

--- frontend/src/pages/Main/index.tsx
+++ frontend/src/pages/Main/index.tsx
@@ -1,13 +1,42 @@
 import React from 'react';
+import { Grid, Image } from 'semantic-ui-react';
 import ProductListing from '../../components/ProductListing';
 
 const Main = () => {
    return (
       <>
-         <h3>Main</h3>
-         <p>Get our newset products</p>
-         <h4>Explore our products</h4>
-         <ProductListing />
+         {/* //TODO: implement real banner */}
+
+         <Grid>
+            <Grid.Row>
+               <Grid.Column>
+                  <Image src={`${process.env.PUBLIC_URL}/main-banner.png`} />
+               </Grid.Column>
+            </Grid.Row>
+            <Grid.Row>
+               <Grid.Column width={5}>
+                  <Image size='medium' src={`${process.env.PUBLIC_URL}/hot-shot.png`} />
+               </Grid.Column>
+               <Grid.Column width={11}>
+                  <hr />
+                  <h2>Hot</h2>
+                  <ProductListing />
+               </Grid.Column>
+            </Grid.Row>
+            <Grid.Row>
+               <Grid.Column>
+                  <hr />
+                  <h2>Promotions</h2>
+                  <ProductListing />
+               </Grid.Column>
+            </Grid.Row>
+            <Grid.Row>
+               <Grid.Column>
+                  <hr />
+                  <Image src={`${process.env.PUBLIC_URL}/newsletter.png`} />
+               </Grid.Column>
+            </Grid.Row>
+         </Grid>
       </>
    )
 }
