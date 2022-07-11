82289f5502484933ef8a87278b8f27e9e05edce5
deliriusz <kalinowskirafal.fm@gmail.com>
2022-07-11 13:08:48 +0200

    adding po tests

--- backend/repository/purchase_test.go
+++ backend/repository/purchase_test.go
@@ -1,9 +1,12 @@
 package repository_test
 
 import (
+	"math/rand"
 	"testing"
+	"time"
 
 	. "github.com/franela/goblin"
+	"rafal-kalinowski.pl/domain/model"
 )
 
 func TestCreatePurchase(t *testing.T) {
@@ -19,5 +22,44 @@ func TestCreatePurchase(t *testing.T) {
 				g.Fail(err)
 			}
 		})
+
+		g.It("Should retrieve created product", func() {
+			g.Timeout(time.Hour)
+			purchase := createRandomPurchase()
+			if err := purchaseRepo.CreatePurchase(purchase); err != nil {
+				g.Fail(err)
+			}
+
+			dbPurchase, err := purchaseRepo.GetPurchase(1)
+			if err != nil {
+				g.Fail(err)
+			}
+
+			g.Assert(dbPurchase.Id == 1).IsTrue()
+			g.Assert(dbPurchase.Date.Unix()).Equal(purchase.Date.Unix())
+			g.Assert(dbPurchase.UserId).Equal(purchase.UserId)
+			g.Assert(dbPurchase.Product[0].ModelId > 0).IsTrue()
+			g.Assert(dbPurchase.Product[0].ModelId).Equal(purchase.Product[0].ModelId)
+
+		})
 	})
+}
+
+func createRandomPurchase() *model.PurchaseOrder {
+	createRandomProductModel()
+	createRandomProductModel()
+	purchaseProduct1 := createRandomProduct(0)
+	purchaseProduct2 := createRandomProduct(0)
+
+	purchaseProduct2.Id += 2
+
+	poTime := time.Now().Add(time.Hour * (time.Duration(-rand.Intn(5000))))
+	return &model.PurchaseOrder{
+		UserId: "asdf",
+		Date:   poTime,
+		Product: []model.Product{
+			*purchaseProduct1,
+			*purchaseProduct2,
+		},
+	}
 }
